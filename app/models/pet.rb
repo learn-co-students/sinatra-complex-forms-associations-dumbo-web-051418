c700699c3b8740745b8caee5b2929f5726133840
github username <github email address>
2018-06-07 08:58:10 -0400

    Finished implementing Pets and Owners form

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,17 @@
 class Pet < ActiveRecord::Base
   belongs_to :owner
+
+  def by_name=(owner)
+    self.owner = Owner.find_or_create_by(name: owner)
+
+    # if owner.class == String
+    #   binding.pry
+    #   @owner = Owner.find(owner.to_i)
+    # else
+    #   @owner = owner
+    # end
+
+    #self.owner_id = owner_id
+    #@owner = Owner.find(owner_id)
+  end
 end
